70b49a08d41430b19141146994b2fe2f5fc44af2
Ed Mark <edmarkangelo.siron@gmail.com>
2024-11-23 08:40:51 +0800

    4th part completed - socket

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuthContext } from "../../context/AuthContext";
-import useConversation from "../../zustand/useConversation";
 import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
@@ -11,28 +11,27 @@ const Message = ({ message }) => {
   const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe
     ? authUser.profilePic
-    : selectedConversation.profilePic;
+    : selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+  const shakeClass = message.shouldShake ? "shake" : "";
 
   return (
     <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img src={profilePic} alt="user avatar" />
+          <img alt="Tailwind CSS chat bubble component" src={profilePic} />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+      <div
+        className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}
+      >
         {message.message}
       </div>
-      <div
-        className={
-          "chat-footer opacity-50 text-xs gap-1 items-center text-white"
-        }
-      >
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
         {formattedTime}
       </div>
     </div>
   );
 };
-
 export default Message;
